f62b89ffec9494eeff19b1c5f4aa6199c33299c7
inesvilla <inesv89@hotmail.com>
2024-01-29 12:21:38 +1000

    latina1

--- main.js
+++ main.js
@@ -1,27 +1,12 @@
-const NOMBRE_DEL_EVENTO = "Evento Interactivo";
+function askToJoinCommunity() {
+    let joinCommunity = confirm("Do you want to be part of the community?");
 
-// array de invitados
-let invitados = ["Juan", "María", "Carlos", "Ana"];
-
-// Mostrar información 
-console.log(`¡Bienvenido al ${NOMBRE_DEL_EVENTO}!`);
-
-// Iterar sobre el array 
-for (let i = 0; i < invitados.length; i++) {
-    // Verificar si el invitado confirmó su asistencia
-    let confirmacion = confirm(`¿${invitados[i]}, confirmas tu asistencia al ${NOMBRE_DEL_EVENTO}?`);
-
-    
-    if (confirmacion) {
-        console.log(`${invitados[i]} asistirá al ${NOMBRE_DEL_EVENTO}.`);
+    if (joinCommunity) {
+        window.location.href = "https://www.instagram.com/latinabeach.au";
     } else {
-        console.log(`${invitados[i]} no podrá asistir al ${NOMBRE_DEL_EVENTO}.`);
+        alert("Enjoy our website !!");
     }
 }
 
-// Verificar el número de invitados que confirmaron asistencia
-if (invitados.length > 0) {
-    alert(`¡Gracias por confirmar tu asistencia al ${NOMBRE_DEL_EVENTO}!`);
-} else {
-    alert(`Lamentablemente, nadie podrá asistir al ${NOMBRE_DEL_EVENTO}.`);
-}
+// Llama a la función cuando se carga el script
+askToJoinCommunity();
